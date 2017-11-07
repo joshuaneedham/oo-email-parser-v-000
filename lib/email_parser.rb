bfb846c02d702f88db24dc70e9b12a6cf45575fd
Joshua Needham <joshuabneedham@gmail.com>
2017-11-07 20:18:10 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
     #   email_addresses = rows.collect do |row|
     #   data = row.split(", ")
     #   email = data[0]
-    # 
+    #
     #   person = self.new
     #   person.email = email
     #   person
